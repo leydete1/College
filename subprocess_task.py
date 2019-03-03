7252f26588f6d6fca42d3902c0cbbb8a6674d8df
leyden100.com <leydete1@LAPTOP-TLLPGO6J.localdomain>
2019-03-03 10:54:19 +0000

    Source upload

--- subprocess_task.py
+++ subprocess_task.py
@@ -1,3 +1,5 @@
 import subprocess
-subprocess.call(["ls", "-l"])
-git clone https://github.com/leydete1/College.git
+subprocess.call("$origin=leydete1@LAPTOP-TLLPGO6J:~/flaskproject/flaskprojectenv/College", shell=True)
+subprocess.call("ls -ltr", shell=True)
+subprocess.call("ls -ltr $origin", shell=True)
+1
