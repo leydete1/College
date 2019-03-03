5934875cf1970d484aaeaf28038ceb32bbc51919
leyden100.com <leydete1@LAPTOP-TLLPGO6J.localdomain>
2019-03-03 11:02:33 +0000

    Sorce upload

--- subprocess_task.py
+++ subprocess_task.py
@@ -1,5 +1,9 @@
 import subprocess
 subprocess.call("$origin=leydete1@LAPTOP-TLLPGO6J:~/flaskproject/flaskprojectenv/College", shell=True)
-subprocess.call("ls -ltr", shell=True)
+subprocess.call("echo Listing repository files" , shell=True)
 subprocess.call("ls -ltr $origin", shell=True)
+subprocess.call("echo Adding files to repository" , shell=True)
+subprocess.call("git add ." , shell=True)
+subprocess.call("echo Commiting repository" , shell=True)
+subprocess.call("git commit -m \"Sorce upload \"" , shell=True)
 1
