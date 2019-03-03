f287568d0fe41106935221db858b87b854d26f97
leyden100.com <leydete1@LAPTOP-TLLPGO6J.localdomain>
2019-03-03 11:07:50 +0000

    Sorce upload

---
+++ upgategit.py
@@ -0,0 +1,10 @@
+import subprocess
+subprocess.call("$origin=leydete1@LAPTOP-TLLPGO6J:~/flaskproject/flaskprojectenv/College", shell=True)
+subprocess.call("echo Listing repository files" , shell=True)
+subprocess.call("ls -ltr $origin", shell=True)
+subprocess.call("cd $origin" , shell=True)
+subprocess.call("echo Adding files to repository" , shell=True)
+subprocess.call("git add ." , shell=True)
+subprocess.call("echo Commiting repository" , shell=True)
+subprocess.call("git commit -m \"Sorce upload \"" , shell=True)
+subprocess.call("git push" , shell=True)
